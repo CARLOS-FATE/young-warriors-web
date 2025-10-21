c7782270f44dcbc3a8d922ff79f3db61e1f63a3b
CARLOS-FATE <xsxcarloslic15@gmail.com>
2025-10-21 01:09:43 -0500

    fix: Actualiza el adaptador de Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+// 👇 CORREGIDO: Esta es la nueva importación
+import vercel from "@astrojs/vercel"; 
 
 // https://astro.build/config
 export default defineConfig({
-  // 👇 AÑADE ESTA LÍNEA AQUÍ
-  site: 'https://young-warriors.vercel.app', 
+  site: 'https://young-warriors.vercel.app',
 
   integrations: [react()],
   output: "server",
