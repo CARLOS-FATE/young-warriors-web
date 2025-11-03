12ed1af71fd110b99b8e6cccc96cf540f0093554
CARLOS-FATE <xsxcarloslic15@gmail.com>
2025-11-03 00:37:16 -0500

    chore: S

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-// Cambio recomendado: importar 'serverless' directamente
-import vercel from "@astrojs/vercel/serverless"; 
+import vercel from "@astrojs/vercel"; // <-- LÍNEA CORREGIDA
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://young-warriors-web.vercel.app', 
-
   integrations: [
     react()
   ],
-  
-  output: "server", // ¡Correcto!
-  
-  adapter: vercel({
-    // Puedes dejar esto vacío si no necesitas opciones especiales
-  }) // ¡Correcto!
-  
+  output: "server",
+  adapter: vercel() 
 });
