8eb8f5b8fb8e722e213f86964fd33ac54d9c0e04
CARLOS-FATE <xsxcarloslic15@gmail.com>
2025-11-16 17:00:54 -0500

    fix: Final hybrid architecture (manual builds + node adapter)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
+import node from "@astrojs/node";
 
 export default defineConfig({
   site: 'https://young-warriors-web.vercel.app', 
   integrations: [
     react()
   ],
-  output: "server" 
-  
+  output: "server" ,
+  adapter: node({  // <-- 2. Usar el adaptador de Node.js
+    mode: "standalone" // Esto es clave para Vercel
+  })
 });
